ac98b913298d57a362acc5906054c4593e75c8eb
Rechimon, Andrés <andres.r1995@hotmail.com>
2024-07-19 15:41:06 -0300

    english and spanish supported

--- storyboard-multilanguage/ViewController.swift
+++ storyboard-multilanguage/ViewController.swift
@@ -7,13 +7,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+let LocalizeUserDefaultKey = "LocalizeUserDefaultKey"
+var LocalizeDefaultLanguage = "es"
 
+class ViewController: UIViewController {
+    @IBOutlet weak var localizedLabel: UILabel!
+    
+    @IBOutlet weak var spanishButton: UIButton!
+    
+    @IBAction func spanishTapped(_ sender: Any) {
+        LocalizeDefaultLanguage = "es"
+        UserDefaults.standard.setValue(LocalizeDefaultLanguage, forKey: LocalizeUserDefaultKey)
+        refreshLanguage()
+    }
+    
+    @IBOutlet weak var englishButton: UIButton!
+    @IBAction func englishTapped(_ sender: Any) {
+        LocalizeDefaultLanguage = "en"
+        UserDefaults.standard.setValue(LocalizeDefaultLanguage, forKey: LocalizeUserDefaultKey)
+        refreshLanguage()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        refreshLanguage()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        LocalizeDefaultLanguage = UserDefaults.standard.string(forKey: LocalizeUserDefaultKey) ?? "es"
     }
 
+    private func refreshLanguage() {
+        localizedLabel.text = "message".translated()
+        spanishButton.setTitle("spanish-btn".translated(), for: .normal)
+        englishButton.setTitle("english-btn".translated(), for: .normal)
+    }
+}
 
+extension String {
+    func translated() -> String {
+        if let path = Bundle.main.path(forResource: LocalizeDefaultLanguage, ofType: "lproj"), let bundle = Bundle(path: path) {
+                    return NSLocalizedString(self, bundle: bundle, comment: "")
+                }
+                
+                return ""
+    }
 }
 
